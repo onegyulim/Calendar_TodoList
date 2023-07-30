d97ef6280acc887c18b7183ef53a9ccd0efb9aea
onegyulim <hahngyulim@naver.com>
2023-07-31 01:01:16 +0900

    Calendar_TodoList

---
+++ 230729_test2/app/src/main/java/com/example/a230729_test2/WeekViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a230729_test2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WeekViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_week_view)
+    }
+}
